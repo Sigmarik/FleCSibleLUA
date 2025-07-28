8a66beda1a7e4e8eec91f8909e74d8c848759f27
Sigmarik <itkudryashov@gmail.com>
2025-07-28 14:58:21 +0300

    feat: rotating box example template

--- examples/RotatingSquare/main.cpp
+++ examples/RotatingSquare/main.cpp
@@ -1,28 +1,42 @@
+#define _USE_MATH_DEFINES
+
 #include <SFML/Graphics.hpp>
+#include <cmath>
 
 int main()
 {
-    // create the window
-    sf::RenderWindow window(sf::VideoMode({800, 600}), "My window");
+    sf::ContextSettings settings;
+    settings.antiAliasingLevel = 8;
+    sf::RenderWindow window(sf::VideoMode({800, 600}), "Rotating Box", sf::Style::Default, sf::State::Windowed, settings);
 
-    // run the program as long as the window is open
+    float boxSize = 200;
+    sf::RectangleShape box(sf::Vector2f(boxSize, boxSize));
+    box.setOrigin(sf::Vector2f(boxSize / 2.0f, boxSize / 2.0f));
+    box.setPosition(sf::Vector2f(400, 300));
+    box.setFillColor(sf::Color(255, 151, 30));
+    box.setOutlineColor(sf::Color(230, 107, 18));
+    box.setOutlineThickness(10);
+
+    box.setRotation(sf::degrees(12.3f));
+
+    sf::Clock clock;
+
     while (window.isOpen())
     {
-        // check all the window's events that were triggered since the last iteration of the loop
+        float dt = clock.restart().asSeconds();
+
         while (const std::optional event = window.pollEvent())
         {
-            // "close requested" event: we close the window
             if (event->is<sf::Event::Closed>())
                 window.close();
         }
 
-        // clear the window with black color
-        window.clear(sf::Color::Black);
+        window.clear(sf::Color(36, 45, 51));
 
-        // draw everything here...
-        // window.draw(...);
+        // TODO: Implement similar behavior with FleCS and LUA
+        box.rotate(sf::degrees(90.0f * dt));
+        window.draw(box);
 
-        // end the current frame
         window.display();
     }
 }
