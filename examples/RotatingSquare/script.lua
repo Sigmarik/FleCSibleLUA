4423db3784f684119f5733cc78cc2ce904bcaaf8
Sigmarik <itkudryashov@gmail.com>
2026-01-02 12:30:22 +0300

    test: fibbonacci sequence generation example

--- examples/RotatingSquare/script.lua
+++ examples/RotatingSquare/script.lua
@@ -1,5 +1,13 @@
-function getGravity() return 9.815 end
+local function fib_generator()
+  local a, b = 0, 1
+  return function()
+    local cur = a
+    a, b = b, a + b
+    return cur
+  end
+end
 
-gravity = getGravity()
-
-f = print('Gravity = ' .. gravity)
+local gen = fib_generator()
+for i = 1, 10 do
+  print(gen()) -- prints first 10 Fibonacci numbers starting 0,1,1,2,...
+end
