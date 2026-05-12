f4603d58242bb912116fc0611b1cd4bc8cb84ed8
Sigmarik <itkudryashov@gmail.com>
2026-05-12 12:19:13 +0300

    feat: ECS performance test

---
+++ perf/ManyObjects/components.h
@@ -0,0 +1,14 @@
+namespace ecs
+{
+struct Position
+{
+    float x = 0;
+    float y = 0;
+};
+
+struct Velocity
+{
+    float x = 0;
+    float y = 0;
+};
+}
--- perf/ManyObjects/main.cpp
+++ perf/ManyObjects/main.cpp
@@ -5,26 +5,37 @@
 #include <flecsible_lua.h>
 #include <iostream>
 
-static constexpr unsigned UPDATE_COUNT = 100;
+#include "components.h"
+
+static constexpr unsigned UPDATE_COUNT = 1000;
+static constexpr unsigned ENTITY_COUNT = 1000;
 
 int main()
 {
     flecs::world world;
 
-    flua::Script script = flua::Script::Load("perf/HeavyInitialization/script.lua");
+    for (unsigned entityIdx = 0; entityIdx < ENTITY_COUNT; entityIdx++)
+    {
+        world.entity().set<ecs::Position>({0, 0}).set<ecs::Velocity>({1, 1});
+    }
+
+    flua::Script script = flua::Script::Load("perf/ManyObjects/script.lua");
+    flua::DeployedScript deployment = script.deploy(world);
     long long executionTime = LLONG_MAX;
     for (unsigned attemptId = 0; attemptId < UPDATE_COUNT; ++attemptId)
     {
         std::chrono::high_resolution_clock::time_point begin = std::chrono::high_resolution_clock::now();
-        flua::DeployedScript deployment = script.deploy(world);
+        world.progress();
         std::chrono::high_resolution_clock::time_point end = std::chrono::high_resolution_clock::now();
         long long attemptTime = std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+        if (attemptTime == 0) continue;
         executionTime = std::min(attemptTime, executionTime);
+
         if (attemptId > 0 && attemptId % 100 == 0)
         {
-            std::cout << "Attempt " << attemptId << "/" << UPDATE_COUNT << ", " << executionTime << " ms" << std::endl;
+            std::cout << "Tick " << attemptId << " / " << UPDATE_COUNT << ", " << attemptTime << " ms" << std::endl;
         }
     }
 
-    std::cout << "\nApproximate script execution time: " << executionTime << " ms" << std::endl;
+    std::cout << "\nApproximate iteration time: " << executionTime << " ms" << std::endl;
 }
